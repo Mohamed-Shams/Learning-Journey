0e0885d50cf1c675387ca1acbf31dddf567ed57b
Mohamed Shams <Mohamed.AH.Shams@gmail.com>
2024-03-01 10:47:11 +0200

    Some Changes

--- NTI/NTI_C_1M/Day 7/Day 7 - 1 (Linked list).c
+++ NTI/NTI_C_1M/Day 7/Day 7 - 1 (Linked list).c
@@ -1,13 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
 
-
 typedef struct node{
   int data;
-  struct list* next;
+  struct node* next;
 }SList;
 
 SList* Create_list();
+SList* InsertAtBeginning(SList* head,int datain);
+void InsertAtEnd(SList* head,int datain);
 
 int main()
 {
@@ -28,21 +29,63 @@ int main()
 
 SList* Create_list()
 {
+  int i,n;
   SList* head = NULL;
-  head = (SList*) malloc(sizeof(SList));
-
-  int datain;
-  if(head == NULL)
+  printf("Please Enter number of nodes: ");
+  scanf("%d",&n);
+  if (n<=0)
   {
-    return NULL;
+    printf("INVALID INPUT");
   }
   else
   {
-    printf("Enter Data No. 1: ");
-    scanf("%d",&datain);
-    head->data = datain;
-    head->next = NULL;
+    int data;
+    printf("Enter the 1st element: ");
+    scanf("%d",&data);
+    head = InsertAtBeginning(head,data);
+    for(i=1;i<n;i++)
+    {
+      printf("Enter Next Data Element: ");
+      scanf("%d",&data);
+      InsertAtEnd(head,data);
+    }
+  }
+  return head;
+}
+
+SList* InsertAtBeginning(SList* head, int datain)
+{
+  SList *temp = (SList*)malloc(sizeof(SList));
+  if (temp == NULL)
+  {
     return head;
   }
+  else
+  {
+    temp -> data = datain;
+    temp -> next = head;
+    head = temp;
+    return head;
+  }
+}
 
+void InsertAtEnd(SList* head, int datain)
+{
+  if (head == NULL)
+  {
+    printf("List is empty");
+      return;
+  }
+  else
+  {
+    SList *temp = (SList*)malloc(sizeof(SList));
+    temp -> data = datain;
+    SList* ptr = head;
+    while(ptr->next != NULL)
+    {
+      ptr = ptr->next;
+    }
+      ptr -> next = temp;
+      temp -> next = NULL;
+  }
 }
