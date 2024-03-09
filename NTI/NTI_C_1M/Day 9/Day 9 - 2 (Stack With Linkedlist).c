45a3c4b02d95b7a7913710fee6fb5263c1e3a539
Mohamed Shams <Mohamed.AH.Shams@gmail.com>
2024-03-09 09:24:46 +0200

    Day 9 - 2 (Stack With Linkedlist)

--- NTI/NTI_C_1M/Day 9/Day 9 - 2 (Stack With Linkedlist).c
+++ NTI/NTI_C_1M/Day 9/Day 9 - 2 (Stack With Linkedlist).c
@@ -1,166 +1,160 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int count =0;
-typedef struct node 
+int count = 0;
+typedef struct node
 {
-  int data ;
-  struct node* next;
-}node_t;
-node_t *top  =NULL;
+  int data;
+  struct node *next;
+} node_t;
+node_t *top = NULL;
 
-int is_stack_is_empty(void);
+int is_stack_empty(void);
 int stack_count(void);
 int top_element(void);
 void print_elements_of_stack(void);
 int pop(void);
 void push(int value);
 
-
 int main()
-{ int choice ,popped_value,pushed_value;
-while(1){
+{
+  int choice, popped_value, pushed_value;
+  while (1)
+  {
 
-     printf("enter your choice :\n");
-        printf("1 to push elements to stack \t 2 to pop element of stack \t 3 to  print element of stack \t 4 to print top value \t 5 to print number of stack \n ");
-        scanf("%i",&choice);
-        printf("\n \n");
-        switch(choice)
-        {
+    printf("enter your choice :\n");
+    printf("1 to push elements to stack \t 2 to pop element of stack \t 3 to  print element of stack \t 4 to print top value \t 5 to print number of stack \t 6 to exit\n ");
+    scanf("%i", &choice);
+    printf("\n \n");
+    switch (choice)
+    {
     case 1:
-        printf("enter value of pushed\n");
-        scanf("%i",&pushed_value);
-        push(pushed_value);
-        break;
+      printf("enter value of pushed\n");
+      scanf("%i", &pushed_value);
+      push(pushed_value);
+      break;
 
     case 2:
 
-        if(is_stack_is_empty()==1)
-        {
-            printf("stack is empty\n");
+      if (is_stack_empty() == 1)
+      {
+        printf("stack is empty\n");
+      }
+      else
+      {
 
-        }
-        else {
-
-         popped_value=pop();
-            printf("poped value is %i\n",popped_value);
-
-
-        }
-        break;
+        popped_value = pop();
+        printf("popped value is %i\n", popped_value);
+      }
+      break;
     case 3:
-        if(is_stack_is_empty()==1)
+      if (is_stack_empty() == 1)
+      {
 
-        {
+        printf("stack is empty\n");
+      }
 
-             printf("stack is empty\n");
-
-
-
-
-        }
-
-    else {
-
+      else
+      {
 
         print_elements_of_stack();
-    }
-    break;
+      }
+      break;
     case 4:
 
-    if(is_stack_is_empty()==1)
-    {
+      if (is_stack_empty() == 1)
+      {
         printf("stack is empty\n");
-    }
-    else {
+      }
+      else
+      {
 
-        printf("value of top is %i\n",top_element);
-    }
-    break;
+        printf("value of top is %i\n", top_element());
+      }
+      break;
     case 5:
-        if(is_stack_is_empty()==1)
-        {
-            printf("stack is empty\n");
-        }
-        else
-        {
-            printf("number of stack elements is %i",stack_count());
-        }
+      if (is_stack_empty() == 1)
+      {
+        printf("stack is empty\n");
+      }
+      else
+      {
+        printf("number of stack elements is %i\n", stack_count());
+      }
 
-        break;
+      break;
     case 6:
-        exit(0);
+      exit(0);
 
     default:
-        printf("worng input\n");
+      printf("wrong input\n");
+    }
+    printf("\n");
+  }
+  return 0;
+}
 
-
-}
-printf("\n");
-}
-    return 0;
-}
-int is_stack_is_empty(void){
-int x;
-if(top=='\0')
+int is_stack_empty(void)
 {
-    x=1;
+  int x;
+  if (top == NULL)
+  {
+    x = 1;
+  }
+  else
+  {
+    x = 0;
+  }
+  return x;
+}
 
-}
-else {
-    x=0;
-}
-return x;
-}
 void push(int value)
 {
-    node_t* temp;
-    if(top=='\0')
-    {
-        top=(node_t*)malloc(sizeof(node_t));
-        top->next='\0';
-        top->data=value;
+  node_t *temp;
+  if (top == NULL)
+  {
+    top = (node_t *)malloc(sizeof(node_t));
+    top->next = NULL;
+    top->data = value;
+  }
+  else
+  {
 
-    }
-else {
-
-    temp=(node_t*)malloc(sizeof(node_t));
-    temp->next=top;
-    temp->data=value;
-    top=temp;
-}
-    count++;
-
+    temp = (node_t *)malloc(sizeof(node_t));
+    temp->next = top;
+    temp->data = value;
+    top = temp;
+  }
+  count++;
 }
 
 int pop(void)
 {
-    int x;
-    node_t *top1=top;
-    top1=top1->next;
-    x=top->data;
-    free(top);
-    count--;
-    return x;
-
-
+  int x;
+  node_t *top1 = top;
+  top1 = top1->next;
+  x = top->data;
+  free(top);
+  count--;
+  return x;
 }
+
 void print_elements_of_stack(void)
 {
-    node_t* top1=top;
-    while(top!='\0')
-    {
-        printf("%i\n",top->data);
-        top1=top1->next;
-    }
-
+  node_t *top1 = top;
+  while (top1 != NULL)
+  {
+    printf("%i\n", top1->data);
+    top1 = top1->next;
+  }
 }
-int top_element(void){
-return top->data;
 
+int top_element(void)
+{
+  return top->data;
 }
-int stack_count(void){
 
-return count;
-
+int stack_count(void)
+{
+  return count;
 }
